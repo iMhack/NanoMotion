3360ad1265df931540d8ad5d0e22af1515cda451
Quentin Talon <quentin.talon@epfl.ch>
2019-09-23 16:03:54 +0200

    Changed the plot Violin to show the step size. Changed also the titles of the plot to show the # we see.

--- single_onmd.py
+++ single_onmd.py
@@ -213,7 +213,8 @@ class Main(QMainWindow, Ui_MainWindow):
     def showResults(self):
         print(self.solver_list)
         for solver in self.solver_list:
-            plot_results(solver.shift_x, solver.shift_y, solver.fps, solver.res, solver.output_name, self.plots_dict)
+            plot_results(shift_x=solver.shift_x, shift_y=solver.shift_y, fps=solver.fps, res=solver.res,
+                         output_name=solver.output_name, plots_dict=self.plots_dict, solver_number=solver.solver_number)
         print("Plots showed")
 
     def export_results(self):
@@ -223,36 +224,38 @@ class Main(QMainWindow, Ui_MainWindow):
         print("Files exported")
 
 
-def plot_results(shift_x, shift_y, fps, res, output_name, plots_dict):
+def plot_results(shift_x, shift_y, fps, res, output_name, plots_dict, solver_number):
     if (plots_dict["x(t)_shift"]):
         plt.figure(num=output_name + 'x(t), um(s)')
         plt.plot([frame / fps for frame in range(len(shift_x))], [x * res for x in shift_x], "-")
         plt.grid()
-        plt.title("x(t)")
+        plt.title("x(t), #"+str(solver_number)+"")
         plt.xlabel("t, s")
         plt.ylabel("x, um")
         plt.savefig(output_name + "_x(t).png")
     if (plots_dict["Violin"]):
         plt.figure(num=output_name + 'violin of shift_x*shift_y')
         shift_length=np.sqrt(np.square(shift_x)+np.square(shift_y))
-        sns.violinplot(data=shift_length)
-        plt.title("violin")
+        shift_step=[shift_length[i + 1] - shift_length[i] for i in range(len(shift_length)-1)]
+        plt.ylabel("step length, um")
+        sns.violinplot(data=shift_step)
+        plt.title("Violin, #"+str(solver_number)+"")
         plt.savefig(output_name + "_violin.png")
 
     if (plots_dict["y(t)_shift"]):
         plt.figure(num=output_name + 'y(t), um(s)')
         plt.plot([frame / fps for frame in range(len(shift_y))], [x * res for x in shift_y], "-")
         plt.grid()
-        plt.title("y(t)")
+        plt.title("y(t), #"+str(solver_number)+"")
         plt.xlabel("t, s")
         plt.ylabel("y, um")
         plt.savefig(output_name + "_y(t).png")
 
     if (plots_dict["pos(t)"]):
-        plt.figure(num='y(x), um(um)')
+        plt.figure(num=output_name + 'y(x), um(um)')
         plt.plot([x * res for x in shift_x], [y * res for y in shift_y], "-")
         plt.grid()
-        plt.title("y(x)")
+        plt.title("y(x), #"+str(solver_number)+"")
         plt.xlabel("x, um")
         plt.ylabel("y, um")
         plt.savefig(output_name + "_y(x).png")
