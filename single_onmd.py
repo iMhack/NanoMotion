653f04e53ee513db1843f596d7830ef950698093
Quentin Talon <quentin.talon@epfl.ch>
2019-09-24 18:36:45 +0200

    Edited a bit the gings to load only once the same frame.

--- single_onmd.py
+++ single_onmd.py
@@ -12,12 +12,14 @@ import pandas as pd
 import seaborn as sns
 import os.path
 from threading import RLock
+
 verrou = RLock()
 from PyQt5.QtWidgets import (QApplication, QFileDialog)
 from PyQt5.uic import loadUiType
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
+
 # To maintain the tips on editing, run pyuic5 mainMenu.ui > mainMenu.py in terminal
 Ui_MainWindow, QMainWindow = loadUiType('mainMenu.ui')
 from mainMenu import (Ui_MainWindow)  # This is used only to have the tips on editing.
@@ -26,6 +28,7 @@ from solver import Solver
 
 config = ConfigParser()
 config.read('settings.ini')
+
 
 class Main(QMainWindow, Ui_MainWindow):
     def __init__(self, ):
@@ -90,17 +93,17 @@ class Main(QMainWindow, Ui_MainWindow):
             for url in e.mimeData().urls():
                 fname = str(url.toLocalFile())
             self.fileName = fname
-            #print(self.fileName+" is the filename")
+            # print(self.fileName+" is the filename")
             self.showFile()
 
     def mouse_event(self, e):
         self.cursor = (e.xdata, e.ydata)
-        #print(e)
+        # print(e)
 
     def plotSelection(self):
         for action in self.menuView_plot.actions():
             self.plots_dict[action.text()] = action.isChecked()
-            #print("menuView is " + action.text() + " " + str(action.isChecked()))
+            # print("menuView is " + action.text() + " " + str(action.isChecked()))
 
     def changefig(self, item):
         text = item.text()
@@ -133,11 +136,11 @@ class Main(QMainWindow, Ui_MainWindow):
         self.showFile()
 
     def showFile(self):  # TODO change name to refer at what this method does
-        #print("showFile()")
+        # print("showFile()")
         try:
             self.videodata = pims.Video(self.fileName)
         except:
-            self.videodata = pims.ImageSequence(self.fileName) # So one can drop a set of images
+            self.videodata = pims.ImageSequence(self.fileName)  # So one can drop a set of images
 
         # self.videodata = pims.MoviePyReader(self.fileName)
         shape = np.shape(self.videodata.get_frame(0))
@@ -145,26 +148,28 @@ class Main(QMainWindow, Ui_MainWindow):
             self.orignalVideoLen = self.videodata._len  # Gives the right with some python environnements or get inf
         except:
             print("Cant get video length")
-        print("Shape of videodata[1] : "+str(shape)+" x "+ str(self.orignalVideoLen)+" frames. Obj type " + str(type(self.videodata)))
-        #print(type(self.videodata))
+        print("Shape of videodata[1] : " + str(shape) + " x " + str(self.orignalVideoLen) + " frames. Obj type " + str(
+            type(self.videodata)))
+        # print(type(self.videodata))
         fig = Figure()
         sub = fig.add_subplot(111)
-        sub.imshow(rgb2gray(self.videodata.get_frame(0)), cmap=plt.cm.gray)
-        #self.addfig('Raw video', fig)
+        self.test = sub.imshow(rgb2gray(self.videodata.get_frame(0)))
+        # self.addfig('Raw video', fig)
         self.basename.append(os.path.basename(self.fileName))
-        self.addfig(self.basename[len(self.basename)-1], fig)
-        self.changefig(self.views.item(0))
+        self.addfig(self.basename[len(self.basename) - 1], fig)
+        self.changefig(self.views.item(0))  # TODO change it for multiple images ?
 
-    def substract(self): # TODO edit it to work with boundaries we set
+    def substract(self):  # TODO edit it to work with boundaries we set
         if self.stopFrame == None: self.stopFrame = self.orignalVideoLen
         fig = Figure()
         sub = fig.add_subplot(111)
-        subset = rgb2gray(self.videodata[int(self.lineEdit_stop_frame.text()) - 1]) - rgb2gray(self.videodata[int(self.lineEdit_start_frame.text())])
+        subset = rgb2gray(self.videodata[int(self.lineEdit_stop_frame.text()) - 1]) - rgb2gray(
+            self.videodata[int(self.lineEdit_start_frame.text())])
         sub.imshow(subset, cmap=plt.cm.gray)
         self.addfig('Subtracted video', fig)
 
     def addDraggableRectangle(self):
-        #print("\naddDraggableRectangle()")
+        # print("\naddDraggableRectangle()")
         w = int(self.lineEdit_w.text())
         h = int(self.lineEdit_h.text())
         if self.cursor[0] is not None and self.cursor[1] is not None:
@@ -175,19 +180,19 @@ class Main(QMainWindow, Ui_MainWindow):
             y0 = 15
         for x in self.fig_dict:
             length = len(self.boxes_dict)
-            print("Adding box "+str(length)+" to figure")
+            print("Adding box " + str(length) + " to figure")
             ax = self.fig_dict[x].add_subplot(111)
-            rect = patches.Rectangle(xy=(x0, y0), width=w, height=h, linewidth=1, edgecolor='r', facecolor='b', fill=False)
+            rect = patches.Rectangle(xy=(x0, y0), width=w, height=h, linewidth=1, edgecolor='r', facecolor='b',
+                                     fill=False)
             ax.add_patch(rect)
             text = ax.text(x=x0, y=y0, s=str(length))
-            #ax.text(x0, y0, str(length))
+            # ax.text(x0, y0, str(length))
             dr = DraggableRectangle(rect, rectangle_number=length, text=text)
             dr.connect()
             self.boxes_dict.append(dr)
             self.boxes.addItem(str(length))
 
     def startAnalysis(self):
-        self.output_name.clear()
         self.solver_list.clear()
         config.set('section_a', 'pix_size', self.lineEdit_pix_size.text())
         config.set('section_a', 'magn', self.lineEdit_magn.text())
@@ -201,36 +206,47 @@ class Main(QMainWindow, Ui_MainWindow):
             config.write(configfile)
         print('Parameters saved')
 
-        for i in range(len(self.boxes_dict)):
-            solver = Solver(videodata=self.videodata, fps=float(self.lineEdit_fps.text()),
-                            w=self.boxes_dict[i].rect._width,
-                            h=self.boxes_dict[i].rect._height, x_rect=int(self.boxes_dict[i].x_rect),
-                            y_rect=int(self.boxes_dict[i].y_rect),
-                            my_upsample_factor=int(self.lineEdit_sub_pix.text()),
-                            stop_frame=int(self.lineEdit_stop_frame.text()),
-                            start_frame=int(self.lineEdit_start_frame.text()),
-                            res=float(self.lineEdit_pix_size.text()),
-                            output_name=create_dirs(self.fileName, str(i)), solver_number=i)
-            self.solver_list.append(solver)
-            self.solver_list[i].progressChanged.connect(self.updateProgress)
-            self.solver_list[i].start()
-        self.fig_dict[self.views.selectedItems()[0].text()].savefig(create_dirs(self.fileName, "boxes_selection")+".png")
+        self.output_name = create_dirs(self.fileName, "")
+        solver = Solver(videodata=self.videodata, fps=float(self.lineEdit_fps.text()),
+                        box_dict=self.boxes_dict, solver_number=0,
+                        my_upsample_factor=int(self.lineEdit_sub_pix.text()),
+                        stop_frame=int(self.lineEdit_stop_frame.text()),
+                        start_frame=int(self.lineEdit_start_frame.text()),
+                        res=float(self.lineEdit_pix_size.text()))
+        self.solver_list.append(solver)
+        self.solver_list[0].progressChanged.connect(self.updateProgress)
+        self.solver_list[0].start()
+        self.fig_dict[self.views.selectedItems()[0].text()].savefig(self.output_name + "boxes_selection.png")
 
-    def updateProgress(self, solver_number, progress):
-        item = self.boxes.item(int(solver_number))
-        item.setText(str(solver_number) + " " + str(progress) + "%")
+    def updateProgress(self, progress, frame, image):
+        for j in range(len(self.boxes_dict)):
+            item = self.boxes.item(j)
+            item.setText(str(j) + " " + str(progress) + "%: f#" + str(frame-int(self.lineEdit_start_frame.text())) + "/"
+                         + str(int(self.lineEdit_stop_frame.text()) - int(self.lineEdit_start_frame.text())))
+
+        self.test.set_data(image)
+        self.fig_dict[self.basename[0]].canvas.draw()
+        self.fig_dict[self.basename[0]].canvas.flush_events()
+
+
 
     def showResults(self):
-        print(self.solver_list)
+        # print(self.solver_list)
         for solver in self.solver_list:
-            plot_results(shift_x=solver.shift_x, shift_y=solver.shift_y, fps=solver.fps, res=solver.res,
-                         output_name=solver.output_name, plots_dict=self.plots_dict, solver_number=solver.solver_number)
+            for j in range(len(solver.box_dict)):
+                plot_results(shift_x=solver.shift_x[j], shift_y=solver.shift_y[j], fps=solver.fps, res=solver.res,
+                             output_name=(self.output_name + str(j)), plots_dict=self.plots_dict, solver_number=j)
         print("Plots showed")
 
     def export_results(self):
         for solver in self.solver_list:
-            export_results(solver.shift_x, solver.shift_y, solver.fps, solver.res, solver.w, solver.h, solver.z_std,
-                           solver.z_rms, solver.v_rms, solver.output_name)
+            for j in range(len(solver.box_dict)):
+                export_results(shift_x=solver.shift_x[j], shift_y=solver.shift_y[j], fps=solver.fps, res=solver.res,
+                               w=solver.box_dict[j].rect._width, h=solver.box_dict[j].rect._height,
+                               z_std=solver.z_std[j],
+                               dz_rms=solver.z_rms[j],
+                               v=solver.v_rms[j],
+                               output_name=self.output_name + str(j))
         print("Files exported")
 
 
@@ -239,25 +255,25 @@ def plot_results(shift_x, shift_y, fps, res, output_name, plots_dict, solver_num
         plt.figure(num=output_name + 'x(t), um(s)')
         plt.plot([frame / fps for frame in range(len(shift_x))], [x * res for x in shift_x], "-")
         plt.grid()
-        plt.title("x(t), #"+str(solver_number)+"")
+        plt.title("x(t), #" + str(solver_number) + "")
         plt.xlabel("t, s")
         plt.ylabel("x, um")
         plt.savefig(output_name + "_x(t).png")
     if (plots_dict["Violin"]):
         plt.figure(num=output_name + 'violin of shift_x*shift_y')
-        shift_x_step=[shift_x[i + 1] - shift_x[i] for i in range(len(shift_x)-1)]
-        shift_y_step=[shift_y[i + 1] - shift_y[i] for i in range(len(shift_y)-1)]
-        shift_length_step=np.sqrt(np.square(shift_x_step)+np.square(shift_y_step))
+        shift_x_step = [shift_x[i + 1] - shift_x[i] for i in range(len(shift_x) - 1)]
+        shift_y_step = [shift_y[i + 1] - shift_y[i] for i in range(len(shift_y) - 1)]
+        shift_length_step = np.sqrt(np.square(shift_x_step) + np.square(shift_y_step))
         plt.ylabel("step length, um")
         sns.violinplot(data=shift_length_step)
-        plt.title("Violin, #"+str(solver_number)+"")
+        plt.title("Violin, #" + str(solver_number) + "")
         plt.savefig(output_name + "_violin.png")
 
     if (plots_dict["y(t)_shift"]):
         plt.figure(num=output_name + 'y(t), um(s)')
         plt.plot([frame / fps for frame in range(len(shift_y))], [x * res for x in shift_y], "-")
         plt.grid()
-        plt.title("y(t), #"+str(solver_number)+"")
+        plt.title("y(t), #" + str(solver_number) + "")
         plt.xlabel("t, s")
         plt.ylabel("y, um")
         plt.savefig(output_name + "_y(t).png")
@@ -266,7 +282,7 @@ def plot_results(shift_x, shift_y, fps, res, output_name, plots_dict, solver_num
         plt.figure(num=output_name + 'y(x), um(um)')
         plt.plot([x * res for x in shift_x], [y * res for y in shift_y], "-")
         plt.grid()
-        plt.title("y(x), #"+str(solver_number)+"")
+        plt.title("y(x), #" + str(solver_number) + "")
         plt.xlabel("x, um")
         plt.ylabel("y, um")
         plt.savefig(output_name + "_y(x).png")
@@ -277,7 +293,7 @@ def export_results(shift_x, shift_y, fps, res, w, h, z_std, dz_rms, v, output_na
     df = pd.DataFrame({"t, s": [frame / fps for frame in range(len(shift_x))], "x, px": shift_x, "y, px": shift_y,
                        "x, um": [x * res for x in shift_x], "y, um": [y * res for y in shift_y]})
     df = pd.concat([df, pd.DataFrame({"z std, um": [z_std], "total z, um": [dz_rms], "v, um/s": [v], "window, px":
-        [str(w) + " x " + str(h)], "window, um": [str(w * res) + " x " + str(h * res)],"um per px": [res]})], axis=1)
+        [str(w) + " x " + str(h)], "window, um": [str(w * res) + " x " + str(h * res)], "um per px": [res]})], axis=1)
     df = df[["t, s", "x, px", "y, px", "x, um", "y, um", "z std, um", "total z, um", "v, um/s", "window, px",
              "window, um", "um per px"]]
     writer = pd.ExcelWriter(
--- solver.py
+++ solver.py
@@ -8,34 +8,29 @@ from decimal import *
 verrou = RLock()
 
 class Solver(QThread):
-    progressChanged = pyqtSignal(int,int)
+    progressChanged = pyqtSignal(int, int, object)
 
-    def __init__(self, videodata, fps, res, w, h, x_rect, y_rect, solver_number, start_frame, stop_frame,
-                 output_name, my_upsample_factor):
+    def __init__(self, videodata, fps, res, box_dict, solver_number, start_frame, stop_frame, my_upsample_factor):
         QThread.__init__(self)
         self.solver_number = solver_number # Stores the ID of the solver
         self.videodata = videodata # Stores an access to the file to get frames
-        self.row_min = None
-        self.row_max = None
-        self.col_min = None
-        self.col_max = None
+        self.row_min = []
+        self.row_max = []
+        self.col_min = []
+        self.col_max = []
         self.fps = fps # Frames per seconds
         self.res = res # Size of pixel (um / pix)
-        self.w = w
-        self.h = h
-        self.x_rect = x_rect
-        self.y_rect = y_rect
         self.my_upsample_factor = my_upsample_factor
         self.start_frame = start_frame
         self.stop_frame = stop_frame
-        self.output_name = output_name
+        self.box_dict = box_dict
 
-        self.z_std = None
-        self.z_rms = None
-        self.v_rms = None
+        self.z_std = []
+        self.z_rms = []
+        self.v_rms = []
 
-        self.shift_x = []
-        self.shift_y = []
+        self.shift_x = [[] for i in range(len(self.box_dict))]
+        self.shift_y = [[] for i in range(len(self.box_dict))]
 
     def run(self):
         self._crop_coord()
@@ -46,73 +41,84 @@ class Solver(QThread):
     def _calcul_phase_corr(self):
         # calculate cross correlation for all frames for the selected  polygon crop
         import time_logging
-        print("row min row max col min col max" + str([self.row_min,self.row_max, self.col_min,self.col_max]))
+        #print("row min row max col min col max" + str([self.row_min,self.row_max, self.col_min,self.col_max]))
+        image_1 = []
         with verrou:
-            image_1 = rgb2gray(
-                self.videodata.get_frame(0)[self.row_min:self.row_max, self.col_min:self.col_max])  # Lock
+            frame_1 = rgb2gray(self.videodata.get_frame(0))
+        for j in range(len(self.box_dict)):
+            image_1.append(rgb2gray(
+                frame_1[self.row_min[j]:self.row_max[j], self.col_min[j]:self.col_max[j]]))
         progress_pivot = 0
-        print(image_1.shape)
+        #print(image_1.shape)
         lenght=self.stop_frame - self.start_frame
-        t = time_logging.start()
+        #t = time_logging.start()
         #t = time_logging.end("Load frame", t)
         for i in range(self.start_frame, self.stop_frame+1):
+            with verrou:
+                frame_n = self.videodata.get_frame(i)
+            for j in range(len(self.box_dict)):
+                image_n = rgb2gray(
+                    frame_n[self.row_min[j]:self.row_max[j], self.col_min[j]:self.col_max[j]])
+                #t = time_logging.end("Load frame", t)
+                shift, error, diffphase = register_translation(image_1[j], image_n, self.my_upsample_factor)
+                #t = time_logging.end("Compute register_translation", t)
+                self.shift_x[j].append(shift[1])
+                self.shift_y[j].append(shift[0])
+
             self.progress = int(((i-self.start_frame) / lenght) * 100)
+            #self.progressChanged.emit(self.progress, i, )
             if self.progress>progress_pivot+4:
-                self.progressChanged.emit(self.solver_number, self.progress)
+                self.progressChanged.emit(self.progress, i, frame_n)
                 print(str(self.progress) + "%: analyse frame " + str(i-self.start_frame) + "/" + str(self.stop_frame-self.start_frame))
                 progress_pivot=self.progress
-                print(time_logging.text_statistics())
-            with verrou:
-                image_2 = rgb2gray(
-                    self.videodata.get_frame(i)[self.row_min:self.row_max, self.col_min:self.col_max])  # Lock
-            # subpixel precision
-            t = time_logging.end("Load frame", t)
-            shift, error, diffphase = register_translation(image_1, image_2, self.my_upsample_factor)
-            t = time_logging.end("Compute register_translation", t)
-            self.shift_x.append(shift[1])
-            self.shift_y.append(shift[0])
-        print(time_logging.text_statistics())
+                #print(time_logging.text_statistics())
+            #print(self.shift_x)
+            #print("\n")
+        #print(time_logging.text_statistics())
 
     def _crop_coord(self):
-        self.row_min = self.y_rect
-        self.row_max = self.y_rect + self.h
-        self.col_min = self.x_rect
-        self.col_max = self.x_rect + self.w
+        for i in range(len(self.box_dict)):
+            self.row_min.append(int(self.box_dict[i].y_rect))
+            self.row_max.append(int(self.box_dict[i].y_rect) + self.box_dict[i].rect._height)
+            self.col_min.append(int(self.box_dict[i].x_rect))
+            self.col_max.append(int(self.box_dict[i].x_rect) + self.box_dict[i].rect._width)
 
     def get_z_std(self):
-        if self.z_std == None:
-            self.z_std = self._z_std_calcul()
-            return self.z_std
-        else:
-            return self.z_std
+        self.z_std = self._z_std_calcul()
+        #print(self.z_std)
+        return self.z_std
 
     def _z_std_calcul(self):
-        x = [i * self.res for i in self.shift_x]
-        y = [i * self.res for i in self.shift_y]
-        z = np.sqrt((np.std(x)) ** 2 + (np.std(y)) ** 2)
-        z_dec = Decimal(str(z))  # special Decimal class for the correct rounding
-        return z_dec.quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)  # rounding, leaves 3 digits after comma
+        z_dec = []
+        for j in range(len(self.box_dict)):
+            x = [i * self.res for i in self.shift_x[j]]
+            y = [i * self.res for i in self.shift_y[j]]
+            z = np.sqrt((np.std(x)) ** 2 + (np.std(y)) ** 2)
+            z_dec.append(Decimal(str(z)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP))  # special Decimal class for the correct rounding
+
+        return z_dec  # rounding, leaves 3 digits after comma
 
     def get_z_delta_rms(self):
-        if self.z_rms is None or self.v_rms is None:
-            self.z_std, self.v_rms = self._z_delta_rms_calcul()
-            return self.z_rms, self.v_rms
-        else:
-            return self.z_rms, self.v_rms
+        self.z_rms, self.v_rms = self._z_delta_rms_calcul()
+        #print(self.z_std, self.v_rms)
+        return self.z_rms, self.v_rms
 
     def _z_delta_rms_calcul(self):  # To modify
-        x = [i * self.res for i in self.shift_x]
-        y = [i * self.res for i in self.shift_y]
-        dx = []
-        dy = []
-        dz = []
-        dv = []
-        for i in range(1, len(x)):
-            dx.append(x[i] - x[i - 1])
-            dy.append(y[i] - y[i - 1])
-            dz.append(np.sqrt(dx[i - 1] ** 2 + dy[i - 1] ** 2))
-        z_tot = np.sum(dz)
-        v = (self.fps / (len(x) - 1)) * z_tot
-        z_tot_dec = Decimal(str(z_tot))  # special Decimal class for the correct rounding
-        v_dec = Decimal(str(v))  # special Decimal class for the correct rounding
-        return z_tot_dec.quantize(Decimal('0.001'), rounding=ROUND_HALF_UP), v_dec.quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)  # rounding, leaves 3 digits after comma
+        z_tot_dec = []
+        v_dec = []
+        for j in range(len(self.box_dict)):
+            x = [i * self.res for i in self.shift_x[j]]
+            y = [i * self.res for i in self.shift_y[j]]
+            dx = []
+            dy = []
+            dz = []
+            dv = []
+            for i in range(1, len(x)):
+                dx.append(x[i] - x[i - 1])
+                dy.append(y[i] - y[i - 1])
+                dz.append(np.sqrt(dx[i - 1] ** 2 + dy[i - 1] ** 2))
+            z_tot = np.sum(dz)
+            v = (self.fps / (len(x) - 1)) * z_tot
+            z_tot_dec.append(Decimal(str(z_tot)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP))  # special Decimal class for the correct rounding
+            v_dec.append(Decimal(str(v)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP))  # special Decimal class for the correct rounding
+        return z_tot_dec, v_dec  # rounding, leaves 3 digits after comma
